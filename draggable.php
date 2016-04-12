9bb596709937867bb0b46570c6b2348930939fce
Denis Godovanets <erami.ukr@gmail.com>
2016-04-12 17:53:17 +0300

    Added sync with server

--- draggable.php
+++ draggable.php
@@ -1,17 +1,24 @@
+
+
 <?php 
 	$x = file_get_contents("data.json");
 	$data = json_decode($x, true);
-	if($_POST["id"] == -1){
-		$data["messages"][] = array("text" => $_POST["text"], "top" => $_POST["top"], "left" => $_POST["left"]);
+	if($_POST["remove"] == "true"){
+		unset($data["messages"][$_POST["id"]]);
 	} else {
-		$id = $_POST["id"];
-		$data["messages"][$id]["text"] = $_POST["text"];
-		$data["messages"][$id]["top"] = $_POST["top"];
-		$data["messages"][$id]["left"] = $_POST["left"];
+		if($_POST["type"] == "create"){
+			$data["messages"][] = array("text" => $_POST["text"], "top" => $_POST["top"], "left" => $_POST["left"], "id" => $_POST["id"]);
+		} else {
+			$id = $_POST["id"];
+			$data["messages"][$id]["text"] = $_POST["text"];
+			if($_POST["top"] != "no"){
+				$data["messages"][$id]["top"] = $_POST["top"];
+				$data["messages"][$id]["left"] = $_POST["left"];
+			}
+		}
+		$x = json_encode($data);
+		file_put_contents("data.json", $x);
 	}
-	$x = json_encode($data);
-	file_put_contents("data.json", $x);
-	echo "Succes";
 ?>
 
 
