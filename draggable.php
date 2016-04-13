b1cba4e015065c374166adab6fe5edc938b64c89
Eramik <erami.ukr@gmail.com>
2016-04-13 19:01:09 +0300

    Bugs fixed

--- draggable.php
+++ draggable.php
@@ -1,30 +1,42 @@
 
 
 <?php 
-	$x = file_get_contents("data.json");
+	/*$x = file_get_contents("data.json");
 	$data = json_decode($x, true);
-	var_dump($_POST["id"]);
-	var_dump(intval($_POST["id"]));
-	if($_POST["remove"] != null){
-		$data["messages"][ intval($_POST["id"])] = null;
-	} else {
-		if($_POST["type"] == "create"){
-			$data["messages"][] = array("text" => $_POST["text"], "top" => $_POST["top"], "left" => $_POST["left"], "id" => $_POST["id"]);
-		} else {
-			$id = intval($_POST["id"]);
-			if($_POST["text"] != null){
-			$data["messages"][$id]["text"] = $_POST["text"];
-			}
-			if($_POST["top"] != null){
-				$data["messages"][$id]["top"] = $_POST["top"];
-			}
-			if($_POST["left"] != null){
-				$data["messages"][$id]["left"] = $_POST["left"];
-			}
-		}
-	}
+		$id = intval($_POST["id"]);
+		$data["messages"][$id]["text"] = $_POST["text"];
+		$data["messages"][$id]["top"] = $_POST["top"];
+		$data["messages"][$id]["left"] = $_POST["left"];
 	$x = json_encode($data);
-	file_put_contents("data.json", $x);
+	file_put_contents("data.json", $x);*/
+	
+	if($_POST["case"] == "create"){
+		$x = file_get_contents("data.json");
+		$data = json_decode($x, true);
+		$data["messages"][] = array("text" => $_POST["text"], "top" => $_POST["top"], "left" => $_POST["left"]);
+		$x = json_encode($data);
+		file_put_contents("data.json", $x);
+	}
+	else if($_POST["case"] == "newpos"){
+		$x = file_get_contents("data.json");
+		$data = json_decode($x, true);
+		print_r( $id = intval($_POST["id"]));
+		$data["messages"][$id]["top"] = $_POST["top"];
+		$data["messages"][$id]["left"] = $_POST["left"];
+		$x = json_encode($data);
+		file_put_contents("data.json", $x);
+	}
+	else if($_POST["case"] == "newval"){
+		$x = file_get_contents("data.json");
+		$data = json_decode($x, true);
+		$id = intval($_POST["id"]);
+		$data["messages"][$id]["text"] = $_POST["text"];
+		$x = json_encode($data);
+		file_put_contents("data.json", $x);
+	}
+	else{ 
+		echo "<p><b>Error</b>: unknown case!</p>";
+	}
 	
 ?>
 
